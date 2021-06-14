ae6e91fb898b983f8bf6bc848da3099f2fa2c963
Will Tao <willtao@live.com>
2021-06-14 08:21:48 -0700

    Update kyria map; MatchDactyl layout to kyria;

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/keymap.c
@@ -41,7 +41,7 @@
 uint16_t copy_paste_timer;
 
 enum custom_keycodes {
-    KC_CCCV = SAFE_RANGE
+    KC_CVZY = SAFE_RANGE // hold-copy/paste; shifted hold-redo/undo
 };
 
 
@@ -54,22 +54,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   UNDO,
+     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
      CTRLESC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
-                     KC_CCCV, KC_EQL,                                                        KC_MINS,SCRNSHT,
-                                     LOWER,  KC_BSPC,                        KC_SPC, TABRSE,
-                                     NAVIG,  ENTLALT,                        DELRALT,NAVIG,
+                     KC_CVZY, KC_EQL,                                                        KC_MINS,SCRNSHT,
+                                     LOWER,  KC_BSPC,                        KC_SPC, RAISE,
+                                     KC_DEL, ENTNAV,                         BSPCNAV,KC_DEL,
                                      KC_LGUI,ADJUST,                         KC_DEL, RUNPRMT
   ),
 
   [_COLEMAKDH] = LAYOUT_5x6(
-     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , UNDO  ,
+     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , _______,
      KC_TAB , KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,                         KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_BSLS,
      CTRLESC, KC_A  , KC_R  , KC_S  , KC_T  , KC_G  ,                         KC_M  , KC_N  , KC_E  , KC_I  , KC_O  ,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                         KC_K  , KC_H  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
-                     KC_CCCV, KC_EQL,                                                        KC_MINS,SCRNSHT,
+                     KC_CVZY, KC_EQL,                                                        KC_MINS,SCRNSHT,
                                      LOWER,  KC_BSPC,                        KC_SPC, TABRSE,
                                      NAVIG,  ENTLALT,                        DELRALT,NAVIG,
                                      KC_LGUI,ADJUST,                         KC_DEL, RUNPRMT
@@ -77,12 +77,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //------------------------------------------------------------------------------------------------------------------------------ 
   // LOWER: Numpad, Function Keys
   [_LOWER] = LAYOUT_5x6(
-     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,REDO,
-     KC_NLCK,KC_SLSH,KC_7,   KC_8,   KC_9,   KC_MINS,                        _______,KC_F7,  KC_F8,  KC_F9, KC_F10,  _______,
-     _______,KC_ASTR,KC_4,   KC_5,   KC_6,   KC_PLUS,                        _______,KC_F4,  KC_F5,  KC_F6, KC_F11,  _______,
-     _______,_______,KC_1,   KC_2,   KC_3,   KC_EQL,                         _______,KC_F1,  KC_F2,  KC_F3, KC_F12,  _______,
+     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     KC_NLCK,KC_SLSH,KC_7,   KC_8,   KC_9,   KC_MINS,                        _______,KC_F9,  KC_F10, KC_F11, KC_F12, KC_PAUS,
+     _______,KC_ASTR,KC_4,   KC_5,   KC_6,   KC_PLUS,                        _______,KC_F5,  KC_F6,  KC_F7,  KC_F8,  _______,
+     _______,KC_0,   KC_1,   KC_2,   KC_3,   KC_EQL,                         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  _______,
                      KC_DOT, KC_0,                                                           _______,_______,
-                                     LOWER,  _______,                        _______,_______,
+                                     LOWER,  KC_SPC,                         _______,_______,
                                      _______,_______,                        _______,_______,
                                      _______,RESET  ,                        KC_PSCR,KC_INS
   ),
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // RAISE Layer: Symbols
   [_RAISE] = LAYOUT_5x6(
 
-     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,REDO,
-     _______,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,                        _______,KC_UNDS,_______,_______,_______,KC_BSLS,
-     _______,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,                         KC_PLUS,KC_MINS,KC_SLSH,KC_ASTR,KC_PERC,KC_QUOT,
-     _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,                        KC_AMPR,KC_EQL, KC_COMM,KC_DOT, KC_SLSH,KC_MINS,
+     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,KC_TILD, KC_AT,   KC_LCBR, KC_RCBR, _______,                    _______,_______,_______,_______,_______,_______,
+     _______,KC_GRV,  KC_DLR,  KC_LPRN, KC_RPRN, KC_EXLM,                    KC_AMPR, KC_MINS, KC_PLUS, KC_EQL,  _______,_______,
+     _______,_______, KC_CIRC, KC_LBRC, KC_RBRC, KC_HASH,                    KC_PIPE, KC_UNDS, KC_SLSH, KC_ASTR, KC_PERC,_______,
                      _______,_______,                                                        _______,_______,
                                      _______,_______,                        _______,RAISE,
                                      _______,_______,                        _______,_______,
@@ -105,24 +105,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NAVIG] = LAYOUT_5x6(
      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
      _______,KC_ACL2,KC_WH_L,KC_MS_U,KC_WH_R,KC_WH_U,                        KC_PGUP,KC_HOME, KC_UP ,KC_END ,_______,KC_SLCK,
-     _______,KC_ACL1,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,                        KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,
-     _______,KC_ACL0,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,KC_ACL1,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,                        KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,_______,KC_CAPS,
+     _______,KC_ACL0,KC_BTN1,KC_BTN3,KC_BTN2,_______,                        _______,_______,_______,_______,_______,KC_INS,
                      _______,_______,                                                        _______,_______,
-                                     KC_BTN1,KC_BTN2,                        _______,_______,
-                                     NAVIG,  KC_BTN3,                        _______,NAVIG,
+                                     _______,_______,                        _______,_______,
+                                     _______,_______,                        _______,_______,
                                      KC_BTN5,KC_BTN4,                        _______,_______
   ),
 
 // ADJUST Layer: RGB, Media
   [_ADJUST] = LAYOUT_5x6(
      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,COLMKDH,
-     _______,_______,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_G,                        _______,KC_MUTE,KC_MPRV,KC_MNXT,_______,_______,
-     _______,_______,RGB_SAI,RGB_HUI,RGB_VAI,RGB_M_SW,                       _______,KC_VOLU,KC_MPLY,_______,_______,_______,
-     _______,_______,RGB_SAD,RGB_HUD,RGB_VAD,RGB_M_K,                        _______,KC_VOLD,_______,_______,_______,_______,
+     _______,_______,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_G,                        _______,_______,_______,_______,_______,KC_VOLU,
+     _______,_______,RGB_SAI,RGB_HUI,RGB_VAI,RGB_M_SW,                       _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLD,
+     _______,_______,RGB_SAD,RGB_HUD,RGB_VAD,RGB_M_K,                        _______,_______,_______,_______,_______,KC_MUTE,
                      _______,RGB_TOG,                                                        _______,_______,
                                      _______,_______,                        _______,_______,
                                      _______,_______,                        _______,_______,
-                                     _______,ADJUST,                         _______,_______
+                                     _______,_______,                        _______,_______
   ),
 };
 
@@ -130,19 +130,73 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+uint16_t cvzy_timer;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_CCCV:  // One key copy/paste
-            if (record->event.pressed) {
-                copy_paste_timer = timer_read();
-            } else {
-                if (timer_elapsed(copy_paste_timer) > TAPPING_TERM) {  // Hold, copy
-                    tap_code16(LCTL(KC_C));
-                } else { // Tap, paste
-                    tap_code16(LCTL(KC_V));
+    switch (keycode) 
+    {
+        case KC_CVZY: // hold-copy/paste; shifted hold-redo/undo
+            if (record->event.pressed) 
+            {
+                cvzy_timer = timer_read();
+            }
+            else
+            {
+                // if shift key is pressed, do undo/redo
+                if ((get_mods() & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) != 0)
+                {
+                    bool is_shift_left = false;
+                    bool is_shift_right = false;
+
+                    // detect and unregister the shift key pressed for undo/redo to only be ctrl + key
+                    if (get_mods() & MOD_BIT(KC_LSFT))
+                    {
+                        is_shift_left = true;
+                        unregister_mods(MOD_BIT(KC_LSFT));
+                    }
+                    
+                    if (get_mods() & MOD_BIT(KC_RSFT))
+                    {
+                        is_shift_right = true;
+                        unregister_mods(MOD_BIT(KC_RSFT));
+                    }
+
+                    if (timer_elapsed(cvzy_timer) > TAPPING_TERM)
+                    {
+                        tap_code16(LCTL(KC_Y));
+                    }
+                    else 
+                    {
+                        tap_code16(LCTL(KC_Z));
+                    }
+                    
+                    // register shift key again
+                    if (is_shift_left)
+                    {
+                        register_mods(MOD_BIT(KC_LSFT));
+                    }
+                    
+                    if (is_shift_right)
+                    {
+                        register_mods(MOD_BIT(KC_RSFT));
+                    }
+                }
+                else
+                {
+                    if (timer_elapsed(cvzy_timer) > TAPPING_TERM) 
+                    {  
+                        // Hold, copy
+                        tap_code16(LCTL(KC_C));
+                    }
+                    else 
+                    { // Tap, paste
+                        tap_code16(LCTL(KC_V));
+                    }
                 }
             }
+
             break;
     }
+
     return true;
 }
